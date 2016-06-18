ca6e1107e79c65e1e9b49f53b8410a1e0746dcce
taxidriver86 <cbryllok@googlemail.com>
2016-06-18 10:34:37 +0000

    MyFirstCppProgramm

---
+++ CPPProjects/FirstProject/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int summandOne;
+    int summandTwo;
+    cout << "Bitte Zahl a und b eingeben:\n";
+    cin >> summandOne >> summandTwo;
+
+    int sum = summandOne+summandTwo;
+    cout << "Ergebnis:" << sum << "\n";
+}
